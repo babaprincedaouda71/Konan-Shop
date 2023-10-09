bda115c79025bcb4d2f58cbd2c0a1975e4a74d0d
babaprincedaouda71 <babadaouda01@gmail.com>
2023-10-09 22:07:35 +0100

    04 09/10/2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {CatalogComponent} from "./catalog/catalog.component";
+import {ProductDetailsComponent} from "./product-details/product-details.component";
 
 const routes: Routes = [
   {path : "home", component : HomeComponent},
   {path : "", component : HomeComponent},
   {path : "catalog", component : CatalogComponent},
   {path : "about", component : AboutComponent},
+  {path : "product-details", component : ProductDetailsComponent},
 ];
 
 @NgModule({
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
-
+  parfumes : Array<any> = [
+    {id : 1, name : "One Million", description : "", price : 50},
+    {id : 2, name : "Savage", description : "", price : 50},
+    {id : 3, name : "Jean Paul Gauthier", description : "", price : 50},
+    {id : 4, name : "Invictus", description : "", price : 50},
+    {id : 5, name : "Ultra Male", description : "", price : 50},
+  ]
 }

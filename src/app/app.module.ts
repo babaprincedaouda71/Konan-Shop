1ea45727880ff2798ccf72fb0c7eaec14118620c
babaprincedaouda71 <babadaouda01@gmail.com>
2023-10-09 01:37:28 +0100

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { PayementComponent } from './payement/payement.component';
+import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { InventoryManagementComponent } from './inventory-management/inventory-management.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    PayementComponent,
+    OrderConfirmationComponent,
+    FooterComponent,
+    NavigationBarComponent,
+    InventoryManagementComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
